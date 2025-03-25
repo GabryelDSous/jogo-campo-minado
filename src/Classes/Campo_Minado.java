041020a6b72a83d7ee07584409b7677a0a237267
Gabryel <gdeusinosousafreitas@gmail>
2025-03-25 18:31:23 -0300

    Classe principal criada

---
+++ src/Classes/Campo_Minado.java
@@ -0,0 +1,13 @@
+package Classes;
+
+public class Campo_Minado {
+
+	public static void main(String[] args) {
+		
+		System.out.println("------------------------");
+		System.out.println("VAMOS JOGAR CAMPO MINADO");
+		System.out.println("------------------------");
+
+	}
+
+}
