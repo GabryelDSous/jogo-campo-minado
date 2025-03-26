40ae645f0b1220691a5bc06a509583842c94049e
Gabryel <gdeusinosousafreitas@gmail>
2025-03-26 17:10:12 -0300

    Jogo Concluido

--- src/Classes/Campo_Minado.java
+++ src/Classes/Campo_Minado.java
@@ -6,12 +6,12 @@ public class Campo_Minado {
 
 	public static void main(String[] args) {
 		/*
-		 * 0 = não escolhido
-		 * 1 = onde tem bomba
-		 * 2 = ja escolhido
-		 * 3 = bomba esplode 
+		 * 0 = não escolhido ?
+		 * 1 = onde tem bomba ?
+		 * 2 = ja escolhido V
+		 * 3 = bomba esplode *
 		 */
-		// INTANCIANDO CLASSE RANDOM
+		// INTANCIANDO CLASSE RANDOM E SCANNER
 		Random random = new Random();
 		Scanner scanner = new Scanner(System.in);
 		
@@ -39,16 +39,20 @@ public class Campo_Minado {
 			}
 		}
 		
+		// ESCREVENDO NA TELA ? ? ? ?
+		for(int l = 0; l < casas.length; l++) {
+			for(int c = 0; c < casas.length; c++) {
+				System.out.print("?\t");
+			}
+			System.out.println("\n");
+		}
+		
 		// JOGANDO
 		int doInicio = 0, tentativas = 0, pontos = 0;
+		boolean ganhou = true;
 		do {
-			// ESCREVENDO NA TELA ? ? ? ?
-			for(int l = 0; l < casas.length; l++) {
-				for(int c = 0; c < casas.length; c++) {
-					System.out.print("?\t");
-				}
-				System.out.println("\n");
-			}
+			
+			System.out.println("----------------------------------");
 			// PEGANDO LINHA E COLUNA
 			int linha, coluna;
 			// Linha
@@ -78,11 +82,44 @@ public class Campo_Minado {
 					System.out.println("Informe uma linha Valida!");
 				}
 			}while(true);
+			System.out.println("----------------------------------");
 			
+			// VERIFICANDO ESCOLHAS IGUAIS
 			
-			// FIM DO JOGO
+			if(casas[linha][coluna] == 2) {
+				System.out.println("Você ja escolheu essa posição!");
+				continue;
+			}
 			tentativas++;
-		}while(tentativas < 5);
+			// MODIFICANDO A LINHA E COLUNA INDICADA
+			for(int l = 0; l < casas.length; l++) {
+				for(int c = 0; c < casas.length; c++) {
+					if(casas[linha][coluna] == 0) casas[linha][coluna] = 2;
+					else if(casas[linha][coluna] == 1) casas[linha][coluna] = 3;
+				}
+				
+			}
+			
+			// MOSTRANDO A LINHA MODIFICADA
+			for(int l = 0; l < casas.length; l++) {
+				for(int c = 0; c < casas.length; c++) {
+					if(casas[l][c] == 0 || casas[l][c] == 1) System.out.print("?\t");
+					else if(casas[l][c] == 2) System.out.print("V\t");
+					else if(casas[l][c] == 3) {
+						System.out.print("*\t");
+						ganhou = false;
+					}
+				}
+				System.out.println("\n");
+			}
+			// FIM DO JOGO
+			pontos+=2;		
+		}while(tentativas < 5 && ganhou);
+		
+		System.out.println("----------------------------------");
+		System.out.println("FIM DE JOGO");
+		System.out.printf("TOTAL DE PONTOS: %d\n", pontos);
+		System.out.printf("TOTAL DE TENTATIVAS: %d\n", tentativas);
+		System.out.println("----------------------------------");
 	}
-
 }
